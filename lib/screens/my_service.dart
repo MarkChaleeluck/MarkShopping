b0e15b2a2556d6659cb19dd2f957bc7cf4267125
Markkii <Chaleeluck.mark@vinpoint.co.th>
2019-07-26 15:58:29 +0700

    Finich

--- lib/screens/my_service.dart
+++ lib/screens/my_service.dart
@@ -2,6 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mark_shopping/screens/home.dart';
 
+import 'page1.dart';
+import 'page2.dart';
+import 'page3.dart';
+
 class Myservice extends StatefulWidget {
   @override
   _MyserviceState createState() => _MyserviceState();
@@ -11,6 +15,8 @@ class _MyserviceState extends State<Myservice> {
   // Explicit
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   String displayNameString = '';
+  Widget myWidget = Page1();
+
   // Method
   @override
   void initState() {
@@ -31,6 +37,9 @@ class _MyserviceState extends State<Myservice> {
       child: ListView(
         children: <Widget>[
           headDrawer(),
+          showPage1(),
+          showPage2(),
+          showPage3(),
           mySignOut(),
         ],
       ),
@@ -41,7 +50,7 @@ class _MyserviceState extends State<Myservice> {
     return DrawerHeader(
       decoration: BoxDecoration(
           gradient: RadialGradient(
-        colors: [Colors.grey, Colors.grey],
+        colors: [Colors.black, Colors.lightBlue],
         center: Alignment.topLeft,
         radius: 1.5,
       )),
@@ -63,23 +72,66 @@ class _MyserviceState extends State<Myservice> {
       title: Text('SignOut'),
       onTap: () {
         processSignOut();
-
       },
     );
   }
-  Future<void> processSignOut ()async{
 
-await firebaseAuth.signOut().then((response){
-  var homeRoute = MaterialPageRoute(builder: (BuildContext context) => Home());
-  Navigator.of(context).pushAndRemoveUntil(homeRoute,(Route<dynamic> route) => false);
-});
+  Widget showPage1() {
+    return ListTile(
+      leading: Icon(Icons.home),
+      title: Text('Page1'),
+      onTap: () {
+        setState(() {
+          myWidget = Page1();
+          Navigator.of(context).pop();
+        });
+        
+      },
+    );
+  }
+
+  Widget showPage2() {
+    return ListTile(
+      leading: Icon(Icons.android),
+      title: Text('Page2'),
+      onTap: () {
+        setState(() {
+          myWidget = Page2();
+          Navigator.of(context).pop();
+        });
+        
+      },
+    );
+  }
+
+  Widget showPage3() {
+    return ListTile(
+      leading: Icon(Icons.access_alarm),
+      title: Text('Page3'),
+      onTap: () {
+        setState(() {
+          myWidget = Page3();
+          Navigator.of(context).pop();
+        });
+        
+      },
+    );
+  }
+
+  Future<void> processSignOut() async {
+    await firebaseAuth.signOut().then((response) {
+      var homeRoute =
+          MaterialPageRoute(builder: (BuildContext context) => Home());
+      Navigator.of(context)
+          .pushAndRemoveUntil(homeRoute, (Route<dynamic> route) => false);
+    });
   }
 
   Widget showLogo() {
     return Container(
       alignment: Alignment.centerLeft,
       height: 80.0,
-      child: Image.asset('images/LOGO.png'),
+      child: Image.asset('images/LOGO2.png'),
     );
   }
 
@@ -96,7 +148,10 @@ await firebaseAuth.signOut().then((response){
   Widget showTopTitle() {
     return Container(
       alignment: Alignment.centerLeft,
-      child: Text('My Service'),
+      child: Text(
+        'My Service',
+        style: TextStyle(color: Colors.white),
+      ),
     );
   }
 
@@ -121,7 +176,7 @@ await firebaseAuth.signOut().then((response){
         backgroundColor: Colors.black,
         title: showTitle(),
       ),
-      body: Text('body'),
+      body: myWidget,
       drawer: mydrawerMenu(),
     );
   }
